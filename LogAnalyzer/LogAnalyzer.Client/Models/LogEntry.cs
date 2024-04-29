202a2eac9626183e5308007c320b52b3e5ef1ada
Vikash Tripathi <tripvik@gmail.com>
2024-04-29 08:57:48 +0530

    Add project files.

---
+++ LogAnalyzer/LogAnalyzer.Client/Models/LogEntry.cs
@@ -0,0 +1,14 @@
+﻿namespace LogAnalyzer.Client.Models
+{
+    public class LogEntry
+    {
+        public DateTime Timestamp { get; set; }
+        public int ProcessId { get; set; }
+        public string ProcessName { get; set; }
+        public string AppDomainId { get; set; }
+        public int ThreadId { get; set; }
+        public string Level { get; set; }
+        public string Logger { get; set; }
+        public string Message { get; set; }
+    }
+}
